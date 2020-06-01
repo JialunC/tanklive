92ce8df3b7b4f8815806d999c6ac29468604d0d7
Tom <jialun.tom.chen@gmail.com>
2020-06-01 00:06:17 -0400

    test

--- entry.sh
+++ entry.sh
@@ -2,7 +2,9 @@
 
 echo Live-stream secret: $1
 
+for i in {1..100000}; do printf "file '%s'\n" /audio/audio.mp3 >> list.txt; done
+
 raspivid -o - -t 0 -w 1920 -h 1080 -fps 40 -b 8000000 -g 40 | ffmpeg \
-    -re -stream_loop -1 -f mp3 -i /audio/audio.mp3 -f h264 \
+    -re concat -i list.txt -f h264 \
     -i pipe:0 -c:v copy -c:a aac -ab 128k -g 40 -strict experimental \
     -f flv -r 30 rtmp://a.rtmp.youtube.com/live2/$1
